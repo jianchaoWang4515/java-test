e166bc88ce52aefcef39ff058753480a3d99b4ae
jianchao.wang <1023024593@qq.com>
2018-10-06 10:13:19 +0800

    fix: 更改配置重新启动完成

--- src/main/java/com/test/LWApplication.java
+++ src/main/java/com/test/LWApplication.java
@@ -3,6 +3,7 @@ package com.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @ComponentScan(value = "com.*")
