cd761ee5c060eea481ca29a13aa2a2567529ca5e
jianchao.wang <1023024593@qq.com>
2018-10-06 10:51:15 +0800

    fix: 优化

--- src/main/java/com/test/LWApplication.java
+++ src/main/java/com/test/LWApplication.java
@@ -3,7 +3,6 @@ package com.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @ComponentScan(value = "com.*")
